bea128e53bdb1c07069f96a03cfb747a382be6de
Abhishek Sirari <abhishek.sirari@comprotechnologies.com>
2024-08-09 13:19:23 +0530

    renaming 'default' node to 'processor' node

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -5,7 +5,7 @@
     <div class="nodes">
       <div class="vue-flow__node-input" :draggable="true" @dragstart="onDragStart($event, 'input')">Input Node</div>
 
-      <div class="vue-flow__node-default" :draggable="true" @dragstart="onDragStart($event, 'default')">Default Node
+      <div class="vue-flow__node-default" :draggable="true" @dragstart="onDragStart($event, 'processor')">Processor Node
       </div>
 
       <div class="vue-flow__node-output" :draggable="true" @dragstart="onDragStart($event, 'output')">Output Node</div>
