7715dcae19868cc66b2fa37bff77188f34386627
Depender Kumar Soni <depender.soni@comprotechnologies.com>
2024-08-27 10:00:39 +0530

    corrected production public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/ai-workflow-creator/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/poc-ai-workflow/" : "/",
   transpileDependencies: true,
   devServer: {
     port: 8000,
